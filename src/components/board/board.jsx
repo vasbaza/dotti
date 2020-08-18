8868ba1c98e8e72d4c4ffc4da9745b23649f10bd
Maria Vasilchenko-Bazarova <maria.bazarom@gmail.com>
2020-08-18 13:29:58 +0300

    sendStep - done

--- src/components/board/board.jsx
+++ src/components/board/board.jsx
@@ -19,78 +19,52 @@ class Board extends React.Component {
         };
     }
 
-    // TODO: Менять сетку и очередность здесь
     sendStep = () => {
         const coordinates = this.state.coordinates;
-        const squares = deepClone(this.state.squares);
-        const player = this.state.player;
         const row = coordinates[0];
         const column = coordinates[1];
 
-        squares[row][column] = player;
-
-        this.setState({squares:squares});
-
-        if (this.state.player === BLUE_PLAYER) {
-            this.setState({player: RED_PLAYER});
-        } else if (this.state.player === RED_PLAYER) {
-            this.setState({player: BLUE_PLAYER});
+        if (row === null || column === null) {
+            alert("Вы не сделали ход!");
+            return;
         }
 
-        this.setState({coordinates: [null, null]});
+        const squares = deepClone(this.state.squares);
+        const player = this.state.player;
+        const state = {};
+
+        squares[row][column] = player;
+
+        if (player === BLUE_PLAYER) {
+            state.player = RED_PLAYER;
+        } else if (player === RED_PLAYER) {
+            state.player = BLUE_PLAYER;
+        }
+
+        state.squares = squares;
+        state.coordinates = [null, null];
+
+        this.setState(state);
     }
 
-    // TODO: Запоминать координаты здесь
     rememberCoordinates = (row, column) => {
         this.setState({coordinates: [row, column]})
     }
 
-    // TODO: Отрисовать фактическую сетку
-    // renderActualBoard = () => {
-    //     const squares = this.state.squares;
-    //     const pointBlue = <div className="pointBlue"></div>;
-    //     const pointRed = <div className="pointRed"></div>;
-    //
-    //     alert('it works');
-    //
-    //     for (let row = 0; row < squares.length; row++) {
-    //         for (let column = 0; column < squares[row].length; column++) {
-    //             if (squares[row][column] === BLUE_PLAYER) {
-    //                 return pointBlue;
-    //             } else if (squares[row][column] === RED_PLAYER) {
-    //                 return pointRed;
-    //             }
-    //         }
-    //     }
-    // }
-
-    // TODO: Изменить состояние квадоатиков и вызвать функцию по запонимнанию координат
     drawPoint = (row, column) => {
         const squares = deepClone(this.state.squares);
 
         if (squares[row][column] === null) {
             this.rememberCoordinates(row, column);
         }
-
-        // this.rememberCoordinates(row, column);
-
-        // if (this.state.player === BLUE_PLAYER) {
-        //     squares[row][column] = BLUE_PLAYER;
-        // } else if (this.state.player === RED_PLAYER) {
-        //     squares[row][column] = RED_PLAYER;
-        // }
-        //
-        // this.setState({squares: squares})
     }
 
     renderPoint = (row,column) => {
         const squares = this.state.squares;
         const player = this.state.player;
-        const pointBlue = <div className="pointBlue"></div>;
-        const pointRed = <div className="pointRed"></div>;
         const coordinates = this.state.coordinates;
-
-        // this.renderActualBoard();
+        const pointBlue = <div className="pointBlue"/>;
+        const pointRed = <div className="pointRed"/>;
 
         if (coordinates[0] === row && coordinates[1] === column) {
             if (player === BLUE_PLAYER) {
